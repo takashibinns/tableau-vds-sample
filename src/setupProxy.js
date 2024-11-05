51f1e8f7deb7667a150ab3d4af1d8696616bd903
Takashi Binns <tbinns@salesforce.com>
2024-11-05 13:04:09 -0500

    make node port match proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'http://localhost:8000',
       changeOrigin: true,
       followRedirects: true // This will likely fix the redirect issue
     })
